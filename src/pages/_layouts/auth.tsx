62ca6059ccc17cf9e499c71a0f1474749c7e7d99
WesleyR10 <wesleyribas2015@gmail.com>
2024-01-10 20:58:35 -0300

    App layout with  header

--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom'
 
 export function AuthLayout() {
   return (
-    <div className="grid min-h-screen grid-cols-2">
+    <div className="grid min-h-screen grid-cols-2 antialiased">
       {/* Ocupar tamanho todo vertical e grid de 2 colunas */}
       <div className="flex h-full flex-col justify-between border-r border-foreground/5 bg-muted p-10 text-muted-foreground">
         {/* foreground - muted - sao cores do próprio shadcn */}
