8997a210a2f69e6975358530c97ed2a61dc1d658
WesleyR10 <wesleyribas2015@gmail.com>
2024-01-10 23:14:04 -0300

    Creating page Order list

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -3,6 +3,7 @@ import * as React from 'react'
 import { cn } from '@/lib/utils'
 
 export interface InputProps
+  // eslint-disable-next-line prettier/prettier
   extends React.InputHTMLAttributes<HTMLInputElement> { }
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
