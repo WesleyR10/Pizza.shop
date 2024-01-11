47f93953ad3a969d1e824ba406315d6bf17104ae
WesleyR10 <wesleyribas2015@gmail.com>
2024-01-10 22:32:00 -0300

    Applying themeToogle and account menu

---
+++ src/components/account-menu.tsx
@@ -0,0 +1,48 @@
+import { Building, ChevronDown, LogOut } from 'lucide-react'
+
+import { Button } from './ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from './ui/dropdown-menu'
+
+export function AccountMenu() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        {/* Devido ter um button dentro do outro passo o asChild pois esse DropdownMenuTrigger é um button */}
+        <Button
+          variant="outline"
+          className="flex select-none items-center gap-2"
+        >
+          Pizza Shop
+          <ChevronDown className="h-4 w-4" />
+        </Button>
+      </DropdownMenuTrigger>
+
+      <DropdownMenuContent align="end" className="w-56">
+        {/* end pois e alinhado com o final do button */}
+        <DropdownMenuLabel className="flex flex-col">
+          {/* DropdownMenuLabel não é clicável */}
+          <span>Diego Fernandes</span>
+          <span className="text-xs font-normal text-muted-foreground">
+            diego@rocketseat.com.br
+          </span>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          <Building className="mr-2 h-4 w-4" />
+          <span>Perfil da loja</span>
+        </DropdownMenuItem>
+        <DropdownMenuItem className="text-rose-500 dark:text-rose-400">
+          <LogOut className="mr-2 h-4 w-4" />
+          <span>Sair</span>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
